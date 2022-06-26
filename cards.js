c70854a7900d74105ee3f9a70e72efcb87e762cb
emma-ni12 <emma.ni2212@gmail.com>
2022-06-26 16:32:31 -0400

    set up boilerplate for html pages, add initial components to home page

---
+++ cards.js
@@ -0,0 +1,21 @@
+const cards = [
+    {
+        name:
+        project:
+        date:
+        role:
+        tools:
+        short-desc:
+        blurb:
+    },
+    {
+        name:
+        project:
+        date:
+        role:
+        tools:
+        short-desc:
+        blurb:
+    },
+
+]
